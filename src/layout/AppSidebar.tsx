f2ad35d4743abece333f67163ad55379c2525f44
Culturepay18 <kenslyeugene@gmail.com>
2026-08-31 17:10:05 -0400

    etat de compte

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -86,7 +86,7 @@ const adminNavItems: NavItem[] = [
   },
   {
     icon: <DocsIcon />,
-    name: "RP Joueurs",
+    name: "État de compte",
     path: "/recus",
     new: true,
     sections: ["Paiements"],
